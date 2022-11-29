313737af42788274bade785b6df86e9d420ddd0c
Dave Richardson <contactme@daverichardson.ca>
2022-11-29 13:27:50 -0600

    initial nextjs installation
    wiped out everything previously and created a new nextjs application.

--- src/api/profileDataApi/index.js
+++
@@ -1,9 +0,0 @@
-import 'whatwg-fetch';
-// import profileData from './profileData.json';
-
-const exposedWordPressAboutPage = '/profile_data.json';
-export const getProfileData = () => fetch(exposedWordPressAboutPage)
-  .then(response => response.json())
-  .then(aboutPageData => {
-    return aboutPageData;
-  });
